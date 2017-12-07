65918f946344d9131f2b97a993574f5636245e59
Mahoney777 <isam126claydene@gmail.com>
2017-12-07 11:35:05 +0000

    Added Files

--- KSApp/__init__.py
+++ KSApp/__init__.py
@@ -1,21 +1,30 @@
 import os
-from flask import Flask
 from volumecap import Harddrive
 from filter import getsevername
 from flask_mysqldb import MySQL
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, flash, redirect, session, url_for, request, logging
+from functools import wraps
+from .forms import RegisterForm
+from passlib.hash import sha256_crypt
+from flask.ext.security import current_user, login_required, RoleMixin, Security, SQLAlchemyUserDatastore, UserMixin, utils
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'mahoney'
-app.config['MYSQL_PASSWORD'] = 'Yb1dAI3vna3xTAMuJNOV'
-app.config['MYSQL_DB'] = 'ksdb'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-mysql = MySQL(app)
+#app.config['MYSQL_HOST'] = 'localhost'
+#app.config['MYSQL_USER'] = 'mahoney'
+#app.config['MYSQL_PASSWORD'] = 'Yb1dAI3vna3xTAMuJNOV'
+#app.config['MYSQL_DB'] = 'ksdb'
+#app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+#mysql = MySQL(app)
 
+app.config['SQLAlchemy_DATABASE_URI'] = 'mysql://mahoney:Yb1dAI3vna3xTAMuJNOV@127.0.0.1:3306/ksdb2'
+
+db = SQLAlchemy(app)
 
 address = getsevername()
 hdd = Harddrive
--- KSApp/views.py
+++ KSApp/views.py
@@ -1,8 +1,37 @@
-from KSApp import app,address,drive,freeSpace,totalSpace, mysql
-from flask import render_template, flash, redirect, session, url_for, request, logging
-from functools import wraps
-from .forms import RegisterForm
-from passlib.hash import sha256_crypt
+from KSApp import *
+#from flask import render_template, flash, redirect, session, url_for, request, logging
+#from functools import wraps
+#from .forms import RegisterForm
+#from passlib.hash import sha256_crypt
+
+
+
+@app.before_first_request
+def before_first_request():
+
+    # Create any database tables that don't exist yet.
+    db.create_all()
+
+    # Create the Roles "admin" and "end-user" -- unless they already exist
+    user_datastore.find_or_create_role(name='admin', description='Administrator')
+    user_datastore.find_or_create_role(name='user', description='basic user account')
+
+    # Create two Users for testing purposes -- unless they already exists.
+    # In each case, use Flask-Security utility function to encrypt the password.
+    encrypted_password = utils.encrypt_password('password')
+    if not user_datastore.get_user('someone'):
+        user_datastore.create_user(username='someone', password=encrypted_password)
+    if not user_datastore.get_user('admin'):
+        user_datastore.create_user(username='admin', password=encrypted_password)
+
+    # Commit any database changes; the User and Roles must exist before we can add a Role to the User
+    db.session.commit()
+
+    # Give one User has the "end-user" role, while the other has the "admin" role. (This will have no effect if the
+    # Users already have these Roles.) Again, commit any database changes.
+    user_datastore.add_role_to_user('someone@example.com', 'user')
+    user_datastore.add_role_to_user('admin@example.com', 'admin')
+    db.session.commit()
 
 
 @app.route('/')
@@ -98,6 +127,14 @@ def admin():
 @is_logged_in
 def addserver():
     return render_template('addserver.html')
+
+
+
+admin = Admin(app)
+
+# Add Flask-Admin views for Users and Roles
+admin.add_view(UserAdmin(User, db.session))
+admin.add_view(RoleAdmin(Role, db.session))
 
 
 
