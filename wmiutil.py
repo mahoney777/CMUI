59bb0e2c49eea0dad56d6b8d699f7de7939bf547
mahoney777 <isam126claydene@gmail.com>
2018-01-03 19:49:56 +0000

    Updated WMI

--- wmiutil.py
+++ wmiutil.py
@@ -1,9 +1,28 @@
 import wmi
+from socket import *
 
-c = wmi.WMI()
+class Connector():
+
+    def __init__(self, ip, username, password):
+        self.ip = ip
+        self.username = username
+        self.password = password
+
+
+    def connect(self, ip, username, password):
+        try:
+            print("Establishing connection to %s" % ip)
+            c = wmi.WMI(ip, user=username, password=password)
+            print("Connection established")
+        except wmi.x_wmi:
+            print("Your Username and Password of " + getfqdn(ip) + " are wrong.")
+
+
+#p = Connector("192.168.31.83", "samAd", "Admin2017")
+#p.connect("192.168.31.83", "samAd", "Admin2017")
 
 class CPU():
-
+    print()
 
 class HDD():
     #Works out the Drive mapping, free space and total space of the Drive.
@@ -27,6 +46,27 @@ class HDD():
 
 
 class RAM():
+    print()
 
 
 class UPDATES():
+    print()
+
+def comd():
+    ip = "192.168.31.83"
+    username = r"192.168.31.83\samAd"
+    password = "Admin2017"
+    try:
+        print("Establishing connection to %s" % ip)
+        connection = wmi.WMI(ip, user=username, password=password)
+        print("Connection established")
+    except wmi.x_wmi:
+        print("Your Username and Password of " + getfqdn(ip) + " are wrong.")
+
+
+def basic():
+    c = wmi.WMI("192.168.31.83", user="samAd", password="Admin2017")
+    for disk in c.Win32_LogicalDisk(DriveType=3):
+        print(disk)
+
+basic()
